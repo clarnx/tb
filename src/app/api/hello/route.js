a548fc365a124f262fa14b75c221ee6a1fcafb24
hubert <huberthalim7@gmail.com>
2023-04-14 16:28:08 +0800

    init

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,3 +1,15 @@
 export async function GET(request) {
   return new Response('Hello, Next.js!')
 }
+
+export async function POST(request) {
+
+  try {
+    let data = await request.formData();
+  } catch (e) {
+    console.log(e)
+    return new Response("fail")
+  }
+
+  return new Response("success")
+}
